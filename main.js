5668e2fe5a8bc16cc40390685a1d2a648383f06c
GitNevo893 <warrior.sharkies@gmail.com>
2026-02-12 12:34:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,15 @@ async function startWebRTC() {
   localStream.getAudioTracks()[0].enabled = false;
 
   pc = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-  });
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
+});
 
   pc.onconnectionstatechange = () => {
     console.log("Connection state:", pc.connectionState);

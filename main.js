7515ce9928233e99ee4b086b3c4ff7728c2d1804
GitNevo893 <warrior.sharkies@gmail.com>
2026-02-12 12:50:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ pc = new RTCPeerConnection({
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     {
-      urls: "turn:openrelay.metered.ca:80",
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
       username: "openrelayproject",
       credential: "openrelayproject"
     }
